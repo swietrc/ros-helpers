461503266a06184b2a5932febd806889a8f7adad
Simon Wietrich <simon.wietrich@gmail.com>
2018-01-24 13:21:34 +0100

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+source /opt/ros/indigo/setup.bash
+
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/
+catkin_make
+catkin_make install
+source devel/setup.bash
+source install/setup.bash
+
+echo $ROS_PACKAGE_PATH
